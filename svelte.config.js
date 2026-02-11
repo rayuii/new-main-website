ddb0ca50cae9181e23a11f933f29894eb8270d39
sundei <sundei@sundei.ee>
2026-02-11 23:49:59 +0100

    L cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
@@ -12,7 +10,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		}),
 		alias: {
 			$lib: './src/lib'
 		}
